4ccf3125bebf16dfc77de1ae3d8a94ffa4b6a775
Scander Mustapha <mustapha.ziy@gmail.com>
2023-01-27 08:43:46 -0500

    fixed a bug in emacs

--- z/packages/emacs.scm
+++ z/packages/emacs.scm
@@ -1,11 +1,19 @@
-(define-module (z packages emacs)
+(define-module (my-channel)
   #:use-module (guix packages)
+  #:use-module (guix download)
   #:use-module (guix utils)
   #:use-module (gnu packages emacs)
+  #:use-module (gnu packages sqlite)
   #:use-module (gnu packages text-editors)
-  #:use-module (guix build-system gnu))
+  #:use-module (gnu packages xorg)
+  #:use-module (gnu packages image)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix git-download)
+  #:use-module (guix licenses)
+  #:export (emacs-next-treesitter))
 
-(define-public emacs
+
+(define-public emacs-next-treesitter
   (package
    (inherit emacs-next)
    (name "emacs-next-treesitter")
@@ -14,5 +22,7 @@
                                   ((#:configure-flags flags ''())
                                    `(cons* "--with-tree-sitter"
                                            ,flags))))
+   
    (inputs (modify-inputs (package-inputs emacs-next)
                           (prepend tree-sitter))))
+  )
